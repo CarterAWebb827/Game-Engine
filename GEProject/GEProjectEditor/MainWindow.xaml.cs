e6a889d287dbc75f50cb5147537056c23cfa6604
CarterAWebb <CarterAWebb827@gmail.com>
2024-02-17 22:59:09 -0800

    Initial App Setup, Open/Create Windows created

---
+++ GEProject/GEProjectEditor/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using GEProjectEditor.GameProject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GEProjectEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+
+            OpenProjectBrowserDialog();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            var projectBrowserDialog = new ProjectBrowserDialg();
+            if (projectBrowserDialog.ShowDialog() == false) // Close the application if the user cancels the dialog
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
